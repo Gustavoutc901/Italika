ad78359e4efac7bca5d60c671c39f25203a55a14
gusgu <gusgu@DESKTOP-6LQOJGH>
2021-03-10 13:09:54 -0600

    Add project files.

---
+++ Italika/Models/validacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Italika.Models
+{
+    [MetadataType(typeof(ProductosMetaData))]
+
+    public partial class Productos
+    {
+        public Tipo TipoC { get; set; }
+    }
+
+    public class ProductosMetaData
+    {
+       
+    }
+}
---
+++ Italika/enumerador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Italika
+{
+    public enum Tipo
+    {
+        Trabajo,
+        Deportiva,
+        Infantin
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetDescripcion(this Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+            if (fieldInfo.CustomAttributes.Count() == 0)
+            {
+                //No tiene atributos personalizados como descripcion
+                return value.ToString();
+            }
+            else
+            {
+                //obtener el valor del atributo Display
+                var attribute = (DisplayAttribute)
+                    fieldInfo.GetCustomAttribute(typeof(DisplayAttribute));
+
+                return attribute.Name;
+            }
+        }
+    }
+}
